a8ac52244edfde396ed9db3f3f0ec679c01fedf9
sanjayadhityaS <sanjayadhitya.saravanan@gcitsolutions.com>
2024-09-10 07:44:12 +0530

    index without style

--- foodapp/view_home.py
+++ foodapp/view_home.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homePage(request):
-    return render(request=request, template_name='home.html')
+    return render(request=request, template_name='foodOnline_template/index.html')
