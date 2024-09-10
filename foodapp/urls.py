744e65cdb8dc6de5e32751b48dfc98cb8bc4a513
sanjayadhityaS <sanjayadhitya.saravanan@gcitsolutions.com>
2024-09-10 06:48:23 +0530

    home view v.1

--- foodapp/urls.py
+++ foodapp/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from foodapp.view_home import homePage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', view=homePage, name='homePage'),
 ]
